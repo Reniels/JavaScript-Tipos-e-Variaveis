6fe69da34d96fcf50f33702702452c3ca7ab8f8e
Alice <148946369+Alicetinha@users.noreply.github.com>
2023-11-23 04:30:29 -0300

    Trhuthy e Falsy

---
+++ trurhy-falsy.js
@@ -0,0 +1,30 @@
+// boolean 
+const usuarioLogado = true;
+const contaPaga = false;
+
+// truthy or falsy
+
+// 0 => false
+// 1 => true
+
+console.log( 0 == true);
+
+console.log("" == false);
+
+console.log(1 == false);
+
+// undefined
+// null 
+
+let minhaVAR;
+let varNull = null;
+
+console.log(minhaVAR);
+console.log(varNull);
+
+
+let numero1 = 3;
+let texto = "alura";
+
+console.log(typeof minhaVAR);
+console.log(typeof varNull);
