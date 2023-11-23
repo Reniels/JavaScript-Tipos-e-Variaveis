e9563415eb05a234e8262cc9beeaf8a4396e596d
Alice <148946369+Alicetinha@users.noreply.github.com>
2023-11-23 04:37:47 -0300

    Conversões

---
+++ conversoes.js
@@ -0,0 +1,21 @@
+// Tipo de dados 
+// booleanos
+
+// conversão implicita
+
+const numero = 456;
+const numeroString = "456a";
+
+// console.log(numero === numeroString);
+// console.log(numero == numeroString);
+//console.log(numero + numeroString);
+
+// Number()
+// String ()
+
+// Conversão explicita
+
+console.log(numero + Number(numeroString));
+console.log(numero == Number(numeroString));
+
+
