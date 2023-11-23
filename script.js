9f315f945d408d976c173a7859d7ba3d43d63d3b
Alice <148946369+Alicetinha@users.noreply.github.com>
2023-11-23 04:03:35 -0300

    var , let , const

---
+++ script.js
@@ -0,0 +1,45 @@
+// var 
+
+/* var altura = 5;
+var comprimento = 7;
+
+var area = altura * comprimento;
+
+console.log(area); */
+
+// let
+
+/* let forma = "retangulo";
+let altura = 5;
+let comprimento = 7;
+let area;
+
+if(forma === "retangulo"){
+
+    area = altura * comprimento;
+
+} else {
+    area = altura * comprimento /2;
+}
+ */
+
+// const
+
+
+const forma = "quadrado";
+const altura = 5;
+const comprimento = 7;
+
+
+if (forma === "quadrado"){
+    area =  altura * comprimento;
+} else {
+    area =  altura * comprimento /2;
+}
+
+console.log(area);
+
+
+
+console.log(area);
+
