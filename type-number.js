6b8e3b938b841864b5d2a0c9c14ee71c3884088c
Alice <148946369+Alicetinha@users.noreply.github.com>
2023-11-23 03:48:26 -0300

    Aula 1

---
+++ type-number.js
@@ -0,0 +1,17 @@
+ // Tipo number
+
+ const meuNumero = 3;
+
+ const primeiroNumero = 1;
+ const segundoNumero = 2;
+
+ const operacaoMatematica = primeiroNumero - segundoNumero;
+
+ console.log(operacaoMatematica);
+ 
+ const pontoFlutuante = 3.3;
+ const pontoFlutuanteSemZero = .3;
+
+ const novaOperacao = pontoFlutuante + pontoFlutuanteSemZero;
+
+ console.log(novaOperacao);
