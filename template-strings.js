87a6ba3e544b0905c374872525073145aaf20ac5
Alice <148946369+Alicetinha@users.noreply.github.com>
2023-11-23 05:24:42 -0300

    Template String

---
+++ template-strings.js
@@ -0,0 +1,7 @@
+const nome = "Alice";
+const idade = 2023 - 1998;
+const cidadeNasc = "Rio de Janeiro";
+// const apresentacao = "meu nome é " + alice + ", minha idade é " + idade + ", nasci no " + cidadeNasc + " .";
+
+const apresentacao = `Meu nome é ${nome} , tenho ${idade} anos , nasci em ${cidadeNasc}`;
+console.log(apresentacao);
