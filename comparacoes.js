3ed9aded4e57ab953fe9d5d1976c1f9506e738b1
Alice <148946369+Alicetinha@users.noreply.github.com>
2023-11-23 05:07:05 -0300

    Comparações

---
+++ comparacoes.js
@@ -0,0 +1,21 @@
+// ==  Comparações implicitas
+
+const numero = 5;
+const numeroString = "5";
+
+console.log(numero == numeroString);
+console.log(numero === numeroString);
+
+// typeof
+
+console.log(typeof numero);
+console.log(typeof numeroString);
+
+// ==   Compara o valor 
+// ==   Compara  tudo  , valor tipo 
+
+// Conversão explicita
+
+Number()
+String()
+
---
+++ console.js
@@ -0,0 +1,11 @@
+// log => registro
+
+minhaVar = true;
+
+console.log(123);
+console.log("coé");
+console.log(minhaVar);
+
+// Tratamento de Erro!
+
+console.error("Deu erro!");
